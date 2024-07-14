608fa05c60060400ed21687358a4a7e43c6afcde
akmeena6 <157712139+akmeena6@users.noreply.github.com>
2024-07-14 13:48:17 +0530

    main file

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello world")
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+housing = pd.read_csv("data.csv")
+train_set, test_set  = train_test_split(housing, test_size=0.2, random_state=42)
+print(f"Rows in train set: {len(train_set)}\nRows in test set: {len(test_set)}\n")
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing['CHAS']):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+housing = strat_train_set.copy()
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+imputer.fit(housing)
+
+X = imputer.transform(housing)
+
+housing_tr = pd.DataFrame(X, columns=housing.columns)
